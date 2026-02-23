e40acafb0b4851d4f6502e88f7e34006fabc36f8
Robinson Reyes <rc.reyesm@uea.edu.ec>
2026-02-22 20:22:42 -0500

    commit

--- app.py
+++ app.py
@@ -1,17 +1,27 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 # Ruta principal
 @app.route('/')
 def inicio():
-    return "Bienvenido al Sistema de Turnos – Clínica XYZ"
+    return render_template('index.html')
 
-# Ruta dinámica
+# Página Acerca de
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+# Ruta dinámica (usuarios o pacientes)
 @app.route('/usuario/<nombre>')
 def usuario(nombre):
-    return f"Bienvenido, {nombre}. Tu turno está en proceso."
+    return render_template('usuario.html', nombre=nombre)
 
-# Ejecutar servidor
+# Ruta de ejemplo: productos
+@app.route('/productos')
+def productos():
+    lista_productos = ["Laptop", "Mouse", "Teclado"]
+    return render_template('productos.html', productos=lista_productos)
+
 if __name__ == '__main__':
     app.run(debug=True)
